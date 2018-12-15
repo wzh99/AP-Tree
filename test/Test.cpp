4eb04880bfac08fd9aba15397e65758d14099493
liuquan98 <liuquan2017@sjtu.edu.cn>
2018-12-15 21:49:11 +0800

    Added primitive test data generation program.

--- test/Test.cpp
+++ test/Test.cpp
@@ -1,1 +1,57 @@
-int main() {}
+#include <iostream>
+#include <ctime>
+#include "../include/aptree.hpp";
+
+using namespace std;
+Query generateQuery(const string k1, const string k2);
+
+int main() 
+{
+	vector<Query> queries;
+
+	srand(time(NULL));
+	queries.push_back(generateQuery("Bruceville", "Cemetery	"));
+	queries.push_back(generateQuery("Boulder", "Agua"));
+	queries.push_back(generateQuery("Sal", "Creek"));
+	queries.push_back(generateQuery("Agua", "Sal"));
+	queries.push_back(generateQuery("Wash", "Aguaje"));
+	queries.push_back(generateQuery("Draw", "Valley"));
+	queries.push_back(generateQuery("Arlington", "State"));
+	queries.push_back(generateQuery("Wildlife", "Area"));
+	queries.push_back(generateQuery("Park", "Bar"));
+
+	vector<string> vocab;
+	vocab.push_back("Agua");
+	vocab.push_back("Aguaje");
+	vocab.push_back("Area");
+	vocab.push_back("Arlington");
+	vocab.push_back("Bar");
+	vocab.push_back("Boulder");
+	vocab.push_back("Bruceville");
+	vocab.push_back("Cemetery");
+	vocab.push_back("Creek");
+	vocab.push_back("Draw");
+	vocab.push_back("Park");
+	vocab.push_back("Sal");
+	vocab.push_back("State");
+	vocab.push_back("Valley");
+	vocab.push_back("Wash");
+	vocab.push_back("Wildlife");
+}
+
+Query generateQuery(const string k1, const string k2){
+	Query obj;
+	obj.keywords.insert(k1);
+	obj.keywords.insert(k2);
+	double x1, x2, y1, y2, x11, x22, y11, y22;
+	x1 = (double)rand() / (double)RAND_MAX;
+	x2 = (double)rand() / (double)RAND_MAX;
+	y1 = (double)rand() / (double)RAND_MAX;
+	y2 = (double)rand() / (double)RAND_MAX;
+	obj.region.min.x = (x1 < x2) ? x1 : x2;
+	obj.region.min.y = (y1 < y2) ? y1 : y2;
+	obj.region.max.x = (x1 < x2) ? x2 : x1;
+	obj.region.max.y = (y1 < y2) ? y2 : y1;
+	
+	return obj;
+}
