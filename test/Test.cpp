6e422241ef8a612a5c81e9d6b854e4a9b26785a0
liuquan98 <liuquan2017@sjtu.edu.cn>
2018-12-15 22:06:47 +0800

    Made up for something I forgot in the last push. Signed-off-by: liuquan98 <liuquan2017@sjtu.edu.cn>

--- test/Test.cpp
+++ test/Test.cpp
@@ -37,21 +37,40 @@ int main()
 	vocab.push_back("Valley");
 	vocab.push_back("Wash");
 	vocab.push_back("Wildlife");
+
+	STObject object1, object2;
+	object1 = genereteObject("Sal", "Agua", "Wildlife");
+	object2 = genereteObject("Bourder", "Draw", "Park");
 }
 
 Query generateQuery(const string k1, const string k2){
 	Query obj;
 	obj.keywords.insert(k1);
 	obj.keywords.insert(k2);
+
 	double x1, x2, y1, y2, x11, x22, y11, y22;
 	x1 = (double)rand() / (double)RAND_MAX;
 	x2 = (double)rand() / (double)RAND_MAX;
 	y1 = (double)rand() / (double)RAND_MAX;
 	y2 = (double)rand() / (double)RAND_MAX;
-	obj.region.min.x = (x1 < x2) ? x1 : x2;
-	obj.region.min.y = (y1 < y2) ? y1 : y2;
+	obj.region.min.x = (x1 < x2) ? x1 : x2;//This may not be able to generate uniform distribution.
+	obj.region.min.y = (y1 < y2) ? y1 : y2;//But I think uniform distribution is not necessary so I keep it :)
 	obj.region.max.x = (x1 < x2) ? x2 : x1;
 	obj.region.max.y = (y1 < y2) ? y2 : y1;
 	
+	return obj;
+}
+
+STObject genereteObject(const string s1, const string s2, const string s3) {
+	STObject obj;
+	obj.keywords.insert(s1);
+	obj.keywords.insert(s2);
+	obj.keywords.insert(s3);
+
+	double xLocation = (double)rand() / (double)RAND_MAX;
+	double yLocation = (double)rand() / (double)RAND_MAX;
+	obj.location.x = xLocation;
+	obj.location.y = yLocation;
+
 	return obj;
 }
